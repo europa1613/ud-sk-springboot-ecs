b006fecbb3f98de9de8d2c9b4ed5f0899fe06a07
Arvind <europa1613@users.noreply.github.com>
2023-12-20 14:21:25 -0600

    remove secure access for h2-console

--- albums-microservice/src/main/java/com/appsdeveloperblog/photoalbums/security/WebSecurity.java
+++ albums-microservice/src/main/java/com/appsdeveloperblog/photoalbums/security/WebSecurity.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -36,8 +37,8 @@ public class WebSecurity {
         http
                 .cors().and()
                 .csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/actuator/**").permitAll()
-                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                 .anyRequest().authenticated().and()
                 .addFilter(new AuthorizationFilter(authenticationManager,environment))
                 .authenticationManager(authenticationManager)
